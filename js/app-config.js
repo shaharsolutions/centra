e3ce07d3456812cbb848eb82baa159f290a8015d
shaharsolutions <shaharsolutions@gmail.com>
2026-03-05 21:16:25 +0200

    reminders update fix

--- js/app-config.js
+++ js/app-config.js
@@ -2,5 +2,5 @@ const CONFIG = {
     supabaseUrl: 'https://qpgceyfsgquhdtvyybwf.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwZ2NleWZzZ3F1aGR0dnl5YndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MzQ3NjAsImV4cCI6MjA4NTUxMDc2MH0.mM2paucqV5IMyYkB2XE-zRkv3i6XxB80dSzTiCXr_3Q',
     calendarCity: 'IL-Tel+Aviv',
-    remindersGasUrl: 'https://script.google.com/macros/s/AKfycbwdggr7vzhYoWijyt56oKr_2rG7ZaqPUpf21731ibK5Xu6e2SuF5svCNGtb2iNOqxLehw/exec' // האדמין יכניס כאן את הכתובת פעם אחת עבור כולם
+    remindersGasUrl: 'https://script.google.com/macros/s/AKfycbxUXL4qgDp5IWNYiQE0EcLWnRDogdhzo9sHi3_w_QB1l-ZjI6W24jtfkIfnDL1FiH5AEA/exec' // האדמין יכניס כאן את הכתובת פעם אחת עבור כולם
 };
